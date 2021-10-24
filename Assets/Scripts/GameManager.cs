34e4f117b0f1699e1ac434e0462ec4b6fd555309
Amanda Grose <agrose071479@live.com>
2021-10-24 18:59:53 -0400

    Splash screen and programming for buttons added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //Singleton set up, instance commonly used
+    public static GameManager instance;
+
+    public GameObject currentObject;
+
+    public void Awake()
+    {
+        instance = this;
+    }
+
+    // If Game Manager is in Scene, Players can presss esc key to quit the game
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
